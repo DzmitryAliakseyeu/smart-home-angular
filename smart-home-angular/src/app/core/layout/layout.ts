5133f7aa3b4084584df48d72160f75145ff863b5
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-12-02 17:53:27 +0100

    chore: add layout component (Tue, 02 Dec, 2025 17:53:01 PM)

---
+++ smart-home-angular/src/app/core/layout/layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'smart-home-layout',
+  imports: [],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss',
+})
+export class Layout {
+
+}
