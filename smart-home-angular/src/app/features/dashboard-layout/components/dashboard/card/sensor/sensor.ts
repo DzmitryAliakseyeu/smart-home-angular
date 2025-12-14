7249428d670ac2eb773694114588e31cdb5b3ab1
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-12-14 14:55:44 +0100

    feat: implementation custom pipe for sensor value (Sun, 14 Dec, 2025 14:55:56 PM)

--- smart-home-angular/src/app/features/dashboard-layout/components/dashboard/card/sensor/sensor.ts
+++ smart-home-angular/src/app/features/dashboard-layout/components/dashboard/card/sensor/sensor.ts
@@ -2,11 +2,12 @@ import { Component, inject, input } from '@angular/core';
 import { CardI, CardItemI } from '../../../../../../core/models/dashboard.model';
 import { AppState } from '../../../../../../state/app-state';
 import {MatIconModule} from '@angular/material/icon';
+import { FormatSensorValuesPipe } from '../../../../pipes/format-sensor-values-pipe';
 
 @Component({
   selector: 'smart-home-sensor',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FormatSensorValuesPipe],
   templateUrl: './sensor.html',
   styleUrls: ['./sensor.scss'],
 })
---
+++ smart-home-angular/src/app/features/dashboard-layout/pipes/format-sensor-values-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatSensorValuesPipe } from './format-sensor-values-pipe';
+
+describe('FormatSensorValuesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatSensorValuesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
