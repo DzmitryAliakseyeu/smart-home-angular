1980a127467f0e4d54158f096809205dbb142f4c
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-12-14 14:40:33 +0100

    feat: implementation custom directive to highlight active device  (Sun, 14 Dec, 2025 14:40:21 PM)

--- smart-home-angular/src/app/features/dashboard-layout/components/dashboard/card/device/device.ts
+++ smart-home-angular/src/app/features/dashboard-layout/components/dashboard/card/device/device.ts
@@ -2,17 +2,18 @@ import { Component, inject, input, Input } from '@angular/core';
 import { CardI, CardItemI } from '../../../../../../core/models/dashboard.model';
 import { AppState } from '../../../../../../state/app-state';
 import { MatIconModule } from '@angular/material/icon';
+import { DeviceHighlight } from "../../../../directives/device-highlight";
 
 
 @Component({
   selector: 'smart-home-device',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, DeviceHighlight],
   templateUrl: './device.html',
   styleUrls: ['./device.scss'],
 })
 export class Device {
   appState = inject(AppState)
- item = input.required<CardItemI>();
- card = input.required<CardI>();
+  item = input.required<CardItemI>();
+  card = input.required<CardI>();
 }
---
+++ smart-home-angular/src/app/features/dashboard-layout/directives/device-highlight.spec.ts
@@ -0,0 +1,8 @@
+import { DeviceHighlight } from './device-highlight';
+
+describe('DeviceHighlight', () => {
+  it('should create an instance', () => {
+    const directive = new DeviceHighlight();
+    expect(directive).toBeTruthy();
+  });
+});
