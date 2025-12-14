b080fb88cde1ec0d5247527b9adb41ee1a6bb410
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-12-14 17:16:28 +0100

    chore: clean gh-pages (Sun, 14 Dec, 2025 17:12:12 PM)

--- smart-home-angular/src/app/features/dashboard-layout/directives/device-highlight.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DeviceHighlight } from './device-highlight';
-
-describe('DeviceHighlight', () => {
-  it('should create an instance', () => {
-    const directive = new DeviceHighlight();
-    expect(directive).toBeTruthy();
-  });
-});
--- smart-home-angular/src/app/features/dashboard-layout/pipes/format-sensor-values-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatSensorValuesPipe } from './format-sensor-values-pipe';
-
-describe('FormatSensorValuesPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatSensorValuesPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
