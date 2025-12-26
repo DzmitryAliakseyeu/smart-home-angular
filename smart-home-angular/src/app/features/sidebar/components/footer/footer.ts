0901de067897115e35a0bde7a8e695c946a92a38
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2025-12-26 16:37:07 +0100

    feat: implementation logout from profile functionality (Fri, Dec 26, 2025 16:31:12 PM)

--- smart-home-angular/src/app/features/sidebar/components/footer/footer.ts
+++ smart-home-angular/src/app/features/sidebar/components/footer/footer.ts
@@ -1,14 +1,25 @@
 import { Component, computed, inject } from '@angular/core';
 import { AuthService } from '../../../../core/services/auth-service/auth-service';
+import { MatIcon } from "@angular/material/icon";
+import { TokenStorage } from '../../../../core/services/token-storage/token-storage';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'smart-home-footer',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
   auth = inject(AuthService);
+  tokenStorage = inject(TokenStorage);
+  router= inject(Router)
 
   userData =  computed(()=>this.auth.userData());
+
+  logout(){
+    this.tokenStorage.clearToken();
+    this.router.navigate(['login']);
+  }
 }
