a1fc4019f3361f10ccf6660112d927546fca0a29
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2026-01-17 20:00:51 +0100

    Auto-generated commit

---
+++ browser/chunk-MESPDWOT.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-ZVNQE7AW.js";import"./chunk-3TBZA5KN.js";import"./chunk-VGNZO7YZ.js";import"./chunk-RIE45KJF.js";export{a as DashboardLayout};
--- chunk-N7PNMXV5.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-4EWQ3BCP.js";import"./chunk-4RCPZBXI.js";import"./chunk-6DIO3INJ.js";import"./chunk-OM3HNEQH.js";export{a as DashboardLayout};
